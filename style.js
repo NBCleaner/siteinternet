3878a134c975dc21590e4d6e81ef44ea6c110833
DelannoyAxel <delannoy.axel2@gmail.com>
2024-05-15 15:51:52 +0200

    push index

--- style.js
+++ style.js
@@ -1,33 +1,91 @@
+function initMap() {
+    let service = new google.maps.places.PlacesService(document.createElement('div.map'));
 
-// const placeId = 'NB+Cleaner+Auto'; 
-// const apiKey = 'AIzaSyD3sS0X_WEsENunWhDPZTW8FI0x2H9WKHU';
+    const request = {
+        placeId: "ChIJzbw80qiN3EcRSTM3nMT40Cc",
+        fields: ["reviews"]
+    };
+    service.getDetails(request, (place, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK && place.reviews) {
+            displayReviewsCarousel(place.reviews);
+        } else {
+            console.error('Impossible de récupérer les avis pour cette entreprise.');
+        }
+    });
+}
 
-// function getPlaceReviews(placeId, apiKey) {
-//     // URL de l'API Google Places pour récupérer les détails de la place
-//     let apiUrl = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + placeId + '&fields=reviews&key=' + apiKey;
+function displayReviewsCarousel(reviews) {
+    let currentIndex = 0; // Indice de l'avis actuellement affiché
 
-//     // Effectuer une requête HTTP GET à l'API
-//     fetch(apiUrl)
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.result && data.result.reviews) {
-//                 // Récupérer les avis de la réponse JSON
-//                 let reviews = data.result.reviews;
+    // Fonction pour créer une carte d'avis
+    function createReviewCard(review) {
+        const reviewCard = document.createElement('div');
+        reviewCard.classList.add('review-card');
 
-//                 // Afficher les avis dans la console
-//                 console.log('Avis du lieu avec ID', placeId);
-//                 reviews.forEach(review => {
-//                     console.log('Avis:', review.text);
-//                     console.log('Note:', review.rating);
-//                 });
-//             } else {
-//                 console.error('Impossible de récupérer les avis pour cette entreprise.');
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Erreur lors de la récupération des avis:', error);
-//         });
-// }
+        const authorName = document.createElement('p');
+        authorName.textContent = `Auteur : ${review.author_name}`;
 
-// // Appeler la fonction pour récupérer les avis
-// getPlaceReviews(placeId, apiKey);
+        const rating = document.createElement('p');
+        rating.textContent = `Note : ${review.rating} étoiles`;
+        
+
+        
+        const starsContainer = document.createElement('div');
+        starsContainer.classList.add('stars-container');
+        for (let i = 0; i < review.rating; i++) {
+            const star = document.createElement('i');
+            star.classList.add('fas', 'fa-star');
+            starsContainer.appendChild(star);
+            
+        }
+        rating.appendChild(starsContainer);
+
+        const text = document.createElement('p');
+        text.textContent = `Avis : ${review.text}`;
+
+        reviewCard.appendChild(authorName);
+        reviewCard.appendChild(rating);
+        reviewCard.appendChild(text);
+
+        return reviewCard;
+    }
+
+    // Fonction pour afficher l'avis à l'indice spécifié
+    function showReview(index) {
+        const reviewContainer = document.querySelector('.review-container');
+        reviewContainer.innerHTML = ''; // Effacer le contenu précédent
+
+        const reviewCard = createReviewCard(reviews[index]);
+        reviewContainer.appendChild(reviewCard);
+    }
+
+    // Fonction pour passer à l'avis suivant
+    function nextReview() {
+        currentIndex++;
+        const reviewsCount = reviews.length;
+        if (currentIndex >= reviewsCount) {
+            currentIndex = 0; // Revenir au premier avis s'il n'y a pas d'avis suivant
+        }
+        showReview(currentIndex);
+    }
+
+    // Fonction pour passer à l'avis précédent
+    function prevReview() {
+        currentIndex--;
+        const reviewsCount = reviews.length;
+        if (currentIndex < 0) {
+            currentIndex = reviewsCount - 1; // Aller au dernier avis s'il n'y a pas d'avis précédent
+        }
+        showReview(currentIndex);
+    }
+
+  
+    document.querySelector('.left-arrow').addEventListener('click', prevReview);
+    document.querySelector('.right-arrow').addEventListener('click', nextReview);
+
+    // Afficher le premier avis au chargement de la page
+    showReview(currentIndex);
+}
+
+window.initMap = initMap;
+
