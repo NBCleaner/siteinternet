63dd532359c8cb2c86fb24a30548661c07e0e74d
ColleMarvin <dev.colle.marvin@gmail.com>
2024-05-29 11:36:35 +0200

    tableau version pc

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 // https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJzbw80qiN3EcRSTM3nMT40Cc
 
-function initMap() {
-    let service = new google.maps.places.PlacesService(document.createElement('div.map'));
+// function initMap() {
+//     let service = new google.maps.places.PlacesService(document.createElement('div.map'));
 
-    const request = {
-        placeId: "ChIJzbw80qiN3EcRSTM3nMT40Cc",
-        fields: ["reviews"]
-    };
-    service.getDetails(request, (place, status) => {
-        place.reviews.forEach(element => {
-            console.log(`nom: ${element.author_name}, note: ${element.rating}, texte: ${element.text}`);
-        });
-    });
-}
+//     const request = {
+//         placeId: "ChIJzbw80qiN3EcRSTM3nMT40Cc",
+//         fields: ["reviews"]
+//     };
+//     service.getDetails(request, (place, status) => {
+//         place.reviews.forEach(element => {
+//             console.log(`nom: ${element.author_name}, note: ${element.rating}, texte: ${element.text}`);
+//         });
+//     });
+// }
 
-window.initMap = initMap;
+// window.initMap = initMap;
