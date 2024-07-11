6d28a1a83e5f553fd87519aad22fc7b5b1d4a52f
DelannoyAxel <delannoy.axel2@gmail.com>
2024-07-11 14:40:56 +0200

    delete old folder

--- old/README.md
+++
@@ -1,1 +0,0 @@
-# siteinternet
--- old/burgerMenu.js
+++
@@ -1,8 +0,0 @@
-// MENU BURGER
-
-const burgerIcon = document.querySelector("#burger-icon");
-
-burgerIcon.addEventListener("click", event => {
-    const navMenu = document.querySelector("nav");
-    navMenu.classList.toggle("shown");
-});
